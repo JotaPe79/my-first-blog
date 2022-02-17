bb68410996fa598cf4822487872bf2da5057f2f6
JotaPe79 <juanpabloalvarezp@gmail.com>
2022-02-17 12:37:46 -0300

    Se agregaron vistas de edit / crear post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+
 from .models import Post
-from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -10,3 +12,38 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_new(request):
+    if request.method == 'POST':
+        # significa que ya se envio la informacion del formulario a traves de post
+        form = PostForm(request.POST)
+        # reconstruyo el formulario con los datos que me llegan a traves de post
+
+        if form.is_valid():
+            post = form.save(commit=False) 
+            # guarda el formulario pero con commit=false todavia no lo ingresa a la BD (falta guardar el usuario)
+            post.author = request.user
+            post.save()
+            #ya esta ingresado el post al sitio
+
+            return redirect('post_detail',pk=post.pk)
+            # redirige a la pagina detalle del nuevo post
+    else:
+        form = PostForm()
+    return render(request,'blog/post_edit.html',{'form':form})
+
+@login_required
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+        return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
